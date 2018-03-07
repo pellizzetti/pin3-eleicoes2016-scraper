e83c2cfa8b2dddc2e53633b8cd522afee44a3e2a
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2018-03-07 01:22:35 -0300

    Remove verificação de url, passa a utilizar regra da spider

--- eleicoes2016_scraper/spiders/eleicoes2016.py
+++ eleicoes2016_scraper/spiders/eleicoes2016.py
@@ -10,19 +10,19 @@ class Eleicoes2016Spider(CrawlSpider):
     name = 'eleicoes2016'
     allowed_domains = ['www.eleicoes2016.com.br']
     start_urls = ['http://www.eleicoes2016.com.br/']
-    rules = (Rule(LxmlLinkExtractor(
-        allow=[r'.*']), callback='parse_obj', follow=True),)
+    rules = [
+        Rule(LxmlLinkExtractor(allow = [r'.*']), callback = 'parse_obj', follow = True),
+        Rule(LxmlLinkExtractor(deny = [r'.+\/candidato.+']), follow = False),
+    ]
 
     def parse_obj(self, response):
-        # Ignora páginas que não são de candidatos
-        if not re.match(r'.+\/candidato.+', response.url):
-            cargo = response.css('div.cargo-upper::text').extract_first() or ''
-            eleito = response.css('div.badge::text').extract_first() == 'Eleito'
+        cargo = response.css('div.cargo-upper::text').extract_first() or ''
+        eleito = response.css('div.badge::text').extract_first() == 'Eleito'
 
-            if (re.match(r'Vereador[a]?', cargo) and eleito):
-                infoCandidato = response.css('div.info-candidato div')
-                nome = infoCandidato[0].css('div::text')[1].extract()
+        if (re.match(r'Vereador[a]?', cargo) and eleito):
+            infoCandidato = response.css('div.info-candidato div')
+            nome = infoCandidato[0].css('div::text')[1].extract()
 
-                yield {
-                    'nome': nome,
-                }
+            yield {
+                'nome': nome,
+            }
